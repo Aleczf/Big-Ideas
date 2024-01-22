95253087f00c8beaaa6d9966f7a6bbb78f9e3072
Aleczf <alessandro.frau88@gmail.com>
2024-01-22 22:10:02 +0100

    Changes to be committed: 	new file:   .DS_Store 	modified:   index.css 	modified:   index.html 	modified:   index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 //  FIREBASE STUFF
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, onValue, push, ref } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, onValue, push, ref, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
     databaseURL: "https://big-ideas-3d5f2-default-rtdb.europe-west1.firebasedatabase.app/"
@@ -12,60 +12,26 @@ const database = getDatabase(app)
 const notesInDB = ref(database, "notes")
 
 
+document.addEventListener('DOMContentLoaded', () => { updateUI() })
 
-document.addEventListener('DOMContentLoaded', () => {
-
+function updateUI() {
     onValue(notesInDB, (snapshot) => {
-        container.innerHTML = ''   //RIPULISCO IL CONTANIER PRIMA DI AGGIUNGERE LE NOTE
 
-        let notes = Object.values(snapshot.val()) //FETCH DELLE NOTE DA FIREBASE 
+        container.innerHTML = ''   //RIPULISCO IL CONTANIER PRIMA DI AGGIUNGERE LE NOTE
+        let notes = Object.values(snapshot.val()) //FETCH DEI VALUES DELLE NOTE DA FIREBASE E CONVERSIONE DELL'OGGETTO IN ARRAY
         
-        for (let nota of notes) {
-            createCardFromFirebase(nota.title, nota.content, nota.uuid)
+        for (const nota of notes) {
+            createCard(nota.title, nota.content, nota.uuid)
         }
     })
-})
-
-
-
-
-function createCardFromFirebase(_title = 'Titolo', _content = '', _uuid) {
-
-    //CREO NUOVA CARD    
-    const newCard = document.createElement('div')
-    newCard.id = 'card'
-    newCard.className = 'card'
-
-    // ASSEGNO UUID SALVATO IN db
-    
-    newCard.setAttribute ('data-uuid', _uuid) 
-
-    //CREO TITOLO DA INSERIRE NELLA CARD
-    const title = document.createElement('h2')
-    title.id = 'title-card'
-    title.className = 'title-card'
-    title.contentEditable = true
-    title.textContent = _title
-    
-    //CREO TEXTAREA DA INSERIRE NELLA CARD
-    const content = document.createElement('div')
-    content.id = 'content-card'
-    content.className = 'content-card'
-    content.contentEditable = true
-    content.textContent = _content
-
-    newCard.appendChild(title)
-    newCard.appendChild(content)
-    container.insertAdjacentElement('afterbegin', newCard)
 }
-
-
 
 
 
 
 const container = document.getElementById('container')
 const overlay = document.getElementById("overlay")
+const card = document.getElementById("card")
 const cardArr = []
 
 
@@ -94,13 +60,26 @@ function createCard(_title = 'Titolo', _content = '') {
     content.contentEditable = true
     content.textContent = _content
 
+    //CREO PULSANTE ELIMINA DA INSERIRE NELLA CARD
+    const deleteBtn = document.createElement('div')
+    deleteBtn.id = 'delete-btn'
+    deleteBtn.className = 'delete-btn'
+    deleteBtn.textContent = "X"
+    deleteBtn.setAttribute('data-uuid', uuid)
+
     newCard.appendChild(title)
     newCard.appendChild(content)
+    newCard.appendChild(deleteBtn)
     container.insertAdjacentElement('afterbegin', newCard)
 
-    saveOnFocusOut(newCard)            // SALVA LA NOTA AL FOCUSOUT
+    saveOnFocusOut(newCard)
+    deleteNote()
+
+    // SALVA LA NOTA AL FOCUSOUT
     
 }
+
+   
 
 
 
@@ -108,6 +87,18 @@ function createCard(_title = 'Titolo', _content = '') {
 document.getElementById('add-btn').addEventListener('click', () => {
     createCard()
 })
+
+
+//  GESTIONE svuota tutto 
+document.getElementById('svuota-tutto').addEventListener('click', () => {
+    
+    onValue(notesInDB, (snapshot) => {
+        container.innerHTML = ''   //RIPULISCO IL CONTANIER PRIMA DI AGGIUNGERE LE NOTE
+        remove(notesInDB)
+        location.reload()
+    })
+})
+
 
 
 
@@ -128,7 +119,6 @@ container.addEventListener('click', (event) => {
 })
 
 
-
 //HANDLER CLICK EXPANDED CARD
 document.addEventListener('click', function(event) {
     const selectedCard = event.target.closest('.card')
@@ -136,9 +126,13 @@ document.addEventListener('click', function(event) {
 
     if(selectedCard){       // ESPANDE LA CARD
         expandCard(selectedCard)
+
+        // document.getElementById("delete-btn").addEventListener('click', function() {
+        //     console.log("removed")
+        // })
         
-        console.log(selectedCard)
-        console.log(`the card ${selectedCard.dataset.uuid} has been selected`)
+        // console.log(selectedCard)
+        // console.log(`the card ${selectedCard.dataset.uuid} has been selected`)
 
     } else if(expandedCard && !event.target.closest('.card')) { //CONTRAE LA CARD
         expandedCard.classList.remove("expanded")
@@ -160,6 +154,20 @@ function saveNote(selectedCard) {
         cardArr[existingCardIndex].title = _title
         cardArr[existingCardIndex].content = _content
 
+        onValue(notesInDB, (snapshot) => {
+            container.innerHTML = ''   //RIPULISCO IL CONTANIER PRIMA DI AGGIUNGERE LE NOTE
+    
+            let notes = Object.values(snapshot.val()) //FETCH DEI VALUES DELLE NOTE DA FIREBASE E CONVERSIONE DELL'OGGETTO IN ARRAY
+            
+            for (const nota of notes) {
+                createCard(nota.title, nota.content, nota.uuid)
+            }
+        })
+
+        console.log('modifica nota')
+        console.log(cardArr)        
+
+
     } else {     // CREA NUOVA NOTA
         const newNote = {
             title : _title,
@@ -168,19 +176,66 @@ function saveNote(selectedCard) {
         }
         cardArr.push(newNote)
         push(notesInDB, newNote)
-
-
     }    
-    console.log(cardArr)        
     return
 }
 
 
-function saveOnFocusOut(myCard) {
-    myCard.addEventListener('focusout', () => {
-        saveNote(myCard)
-    })
 
+function saveOnFocusOut(myCard) {
+    myCard.addEventListener('focusout', (event) => {
+        const isDeleteButton = event.relatedTarget && event.relatedTarget.id === 'delete-btn';
+        const isOverlay = event.relatedTarget && event.relatedTarget.id === 'overlay';
+
+        if (!isDeleteButton && !isOverlay) {
+            // Se il focusout non è stato causato dal click sul pulsante di eliminazione o sull'overlay, salva la nota
+            saveNote(myCard);
+            updateUI();
+        }
+
+        const expandedCard = document.querySelector('.card.expanded');
+        if (!expandedCard) {
+            overlay.style.display = "none";
+        }
+    });
 }
 
+
+
+
+function deleteNote(){
+
+    const deleteBtns = document.querySelectorAll('.delete-btn')
+
+    deleteBtns.forEach(deleteBtn => {
+        deleteBtn.addEventListener('click', (event) => {
+            event.stopImmediatePropagation() 
+    
+            const uuid = deleteBtn.getAttribute('data-uuid')
+            const selectedCard = event.target.closest('.card')     
+            console.log('rimosso')
+    
+            if (selectedCard){     
+                selectedCard.remove()
+
+                onValue(notesInDB, (snapshot) => {
+                let notes = Object.values(snapshot.val()) //FETCH DEI VALUES DELLE NOTE DA FIREBASE E CONVERSIONE DELL'OGGETTO IN ARRAY
+
+
+                    for (let i = 0; i < notes.length; i++) {
+                        console.log(notes)
+
+                        let currentItem = notes[i]
+                        let currentItemID = currentItem[0]
+                        console.log(currentItemID)
+
+                    const locationInDB = ref(database, `notes/${uuid}`)
+                    remove(locationInDB)
+                    }
+                })
+            }
+        })    
+
+    })
+}
 
