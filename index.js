f0507f2b6ea3929545d5e2954bdd1a02664ff351
Aleczf <alessandro.frau88@gmail.com>
2024-02-01 11:44:30 +0100

    Changes to be committed: 	modified:   index.css 	modified:   index.html 	modified:   index.js
         Primo commit con tutte le features funzionanti

--- index.js
+++ index.js
@@ -23,15 +23,16 @@ let cardArr = []
 onValue(notesInDB, (snapshot) => {
 
     container.innerHTML = ''   //RIPULISCO IL CONTANIER PRIMA DI AGGIUNGERE LE NOTE
-    notes = Object.values(snapshot.val()) //FETCH DEI VALUES DELLE NOTE DA FIREBASE E CONVERSIONE DELL'OGGETTO IN ARRAY
-    notesArray = Object.entries(snapshot.val())
-    console.log(notesArray)
-
-    updateUI()
     
+    const snapshotValue = snapshot.val() 
+    if(snapshotValue){ //MI ASSICURO CHE IL DATABASE (SNAPSHOT) SIA POPOLATO
+        
+        notes = Object.values(snapshotValue) //FETCH DEI VALUES DELLE NOTE DA FIREBASE E CONVERSIONE DELL'OGGETTO IN ARRAY
+        notesArray = Object.entries(snapshotValue) //FETCH DELL'INTERO ARRAY CON CHIAVI E VALORI DELLE NOTE DA FIREBASE E CONVERSIONE DELL'OGGETTO IN ARRAY        
+        console.log(notesArray)
+        updateUI()
+    }    
 })
-
-
 
 
 function updateUI() {
@@ -113,18 +114,16 @@ document.getElementById('svuota-tutto').addEventListener('click', () => {
 })
 
 
-
-
 // FUNZIONE PER ESPANSIONE DELLA CARD
-
 function expandCard(card) {
     let uuid = card.getAttribute('data-uuid')
-    if (!uuid) {
-        // Se la card non ha un UUID, generane uno e assegnalo
-        uuid = Math.floor(Math.random() * 10000000)
-        card.setAttribute('data-uuid', uuid)
-        console.log(card.uuid)
-    }
+
+    // if (!uuid) {
+    //     // Se la card non ha un UUID, generane uno e assegnalo
+    //     uuid = Math.floor(Math.random() * 10000000)
+    //     card.setAttribute('data-uuid', uuid)
+    //     console.log(card.uuid)
+    // }
 
     card.classList.add("expanded")
     overlay.style.display = "block" 
@@ -156,14 +155,6 @@ document.addEventListener('click', function(event) {
 
 
 
-// function editFirebaseNote(editedNote){
-//     const noteRef = ref(database, `notes/${editedNote.uuid}`)
-//     set(noteRef, editedNote)
-//     console.log('Nota aggiornata su Firebase')
-// }
-
-
-
 // SALVO NUOVA NOTA O MODIFICO SE PREESISTENTE
 function saveNote(selectedCard) {
 
@@ -171,26 +162,20 @@ function saveNote(selectedCard) {
     let _title = selectedCard.querySelector('.title-card').innerText
     let _content = selectedCard.querySelector('.content-card').innerText
     
-    const existingCard = notesArray.find((card) => card[1].uuid === _uuid)
-    console.log(existingCard)
+    const existingCard = notesArray.find((card) => card[1].uuid === _uuid) //VERIFICO SE LA NOTA è GIA ESISTENTE
 
     if(existingCard) {
-        let noteToEdit = existingCard[1]
-        console.log(noteToEdit)
         
         let noteEdited = {
             title : _title,
             content : _content,
             uuid : _uuid
         }
-        console.log(noteEdited)
-
 
         let noteIDToEdit = existingCard[0]
-
         let exactLocationOfItemInDB = ref(database, `notes/${noteIDToEdit}`)
+        
         set(exactLocationOfItemInDB, noteEdited)
-
 
     } else {     // CREA NUOVA NOTA
         const newNote = {
@@ -198,13 +183,8 @@ function saveNote(selectedCard) {
             content : _content,
             uuid : _uuid
         }
-        cardArr.push(newNote)
-        console.log('nuova nota')
-
         push(notesInDB, newNote) 
-
     }    
-    return
 }
 
 
@@ -212,19 +192,12 @@ function saveNote(selectedCard) {
 function saveOnFocusOut(myCard, _uuid) {
 
     myCard.addEventListener('focusout', (event) => {
-        console.log('focusout innescato')
 
         const isDeleteButton = event.relatedTarget && event.relatedTarget.id === 'delete-btn';
         const isOverlay = event.relatedTarget && event.relatedTarget.id === 'overlay';
 
-        if (!isDeleteButton && !isOverlay) {
-        
-            // Se il focusout non è stato causato dal click sul pulsante di eliminazione o sull'overlay, salva la nota
-        
-            // _uuid = myCard.getAttribute('data-uuid')
-        
+        if (!isDeleteButton && !isOverlay) {  // Se il focusout non è stato causato dal click sul pulsante di eliminazione o sull'overlay, salva la nota
             saveNote(myCard, _uuid);
-            // updateUI();
         }
 
         const expandedCard = document.querySelector('.card.expanded');
@@ -237,35 +210,24 @@ function saveOnFocusOut(myCard, _uuid) {
 
 
 function deleteNote(){
-
+    
     const deleteBtns = document.querySelectorAll('.delete-btn')
-
     deleteBtns.forEach(deleteBtn => {
         deleteBtn.addEventListener('click', (event) => {
-            event.stopImmediatePropagation() 
-    
+
+            event.stopImmediatePropagation()     
+
             const uuid = deleteBtn.getAttribute('data-uuid')
-            // const selectedCard = event.target.closest('.card')     
-    
-                onValue(notesInDB, (snapshot) => {
-                    
-                    if (notesInDB) {
-                        let noteIDToDelete
-                        let notesArrFromDB = Object.entries(snapshot.val()) //FETCH DEI VALUES DELLE NOTE DA FIREBASE E CONVERSIONE DELL'OGGETTO IN ARRAY
-                        
-                        for (let nota of notesArrFromDB){
+            let noteIDToDelete
 
-                            if(nota[1].uuid === uuid){
-                                console.log(nota[0])
-                                noteIDToDelete = nota[0]
-                            } 
+            for (let nota of notesArray){
 
-                            let exactLocationOfItemInDB = ref(database, `notes/${noteIDToDelete}`)
-                            remove(exactLocationOfItemInDB)                                     
-                        
-                        }                        
-                    }
-                })            
+                if(nota[1].uuid == uuid){ //USO DI EQUALITY NON STRICT IN QUANTO COMPARAZIONE TRA STRINGA E NUMERO
+                    noteIDToDelete = nota[0]  
+                }
+                let exactLocationOfItemInDB = ref(database, `notes/${noteIDToDelete}`)
+                remove(exactLocationOfItemInDB)                                                 
+            }              
         })    
     })
 }
@@ -273,54 +235,3 @@ function deleteNote(){
 
 
 
-
-
-
-// let arr = [
-    
-//         {
-//             0: "432432",
-//             1: {
-//                 titolo: 'abc',
-//                 content: 'def',
-//                 uuid: 153167816
-//             }
-//         },
-//         {
-//             0: "5381832",
-//             1: {
-//                 titolo: 'abc',
-//                 content: 'def',
-//                 uuid: 789468
-//             }
-//         },
-//         {
-//             0: "08161",
-//             1: {
-//                 titolo: 'abc',
-//                 content: 'def',
-//                 uuid: 123456
-//             }
-//         },
-//         {
-//             0: "86414684",
-//             1: {
-//                 titolo: 'abc',
-//                 content: 'def',
-//                 uuid: 4534186
-//             }
-//         }
-// ]
-
-// const uuidProva = 123456
-
-// let prova = arr.find((item) => trovaUuid(item))
-
-// function trovaUuid(item) {
-//     console.log(item[1].uuid)
-//     return item[1].uuid === uuidProva    
-// }
-
-
-
-// console.log(prova)
